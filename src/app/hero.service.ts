f52562c8a53e805e9e1656f09d9a83254d795067
mbernasinski <contact@mbernasinski.com>
2021-01-28 15:02:52 +0100

    V6.1 - HeroService.getHeroes avec le serveur HTTP
    HeroService.getHeroes appelle maintenant notre serveur HTTP pour récupérer la liste des héros
    + Log des informations dans une méthode dédiée

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -8,16 +10,24 @@ import { MessageService } from './message.service';
     providedIn: 'root'
 })
 export class HeroService {
+    private heroesUrl = 'api/heroes';  // URL to web api
 
-    constructor(private messageService: MessageService) { }
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService) { }
 
     getHeroes(): Observable<Hero[]> {
-        this.messageService.add('HeroService: fetched heroes');
-        return of(HEROES);
-    }
+        this.log('fetched heroes');
+        return this.http.get<Hero[]>(this.heroesUrl);
+      }
 
     getHero(id: number): Observable<Hero> {
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
+        this.log(`fetched hero id=${id}`);
         return of(HEROES.find(hero => hero.id === id));
+    }
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string): void {
+        this.messageService.add(`HeroService: ${message}`);
     }
 }
